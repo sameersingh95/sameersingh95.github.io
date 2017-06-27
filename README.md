8fb0cc27be1400fdbd7581be48727b9117547888
Sameer Singh <sshanu95@gmail.com>
2017-06-27 13:33:42 +0530

    upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# PHP-Website-Using-Bootstrap
+This is the updated version of the previous website . Now using bootstrap.
+Totally revamped front end of the website.
+Enjoy
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION["signin"]);
-unset($_SESSION["username"]);
-session_destroy();
-session_unset();
-echo("<meta http-equiv=\"refresh\" content=\"0;URL='Home.php'\">");
-?>
